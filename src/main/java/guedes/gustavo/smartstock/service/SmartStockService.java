5e6bec52ab1c1eacba2568645de0bded6feaf710
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-11-21 12:39:54 -0300

    feat: add report path and reorder quantity calculation in SmartStockService

--- src/main/java/guedes/gustavo/smartstock/service/SmartStockService.java
+++ src/main/java/guedes/gustavo/smartstock/service/SmartStockService.java
@@ -1,5 +1,6 @@
 package guedes.gustavo.smartstock.service;
 
+import guedes.gustavo.smartstock.domain.CsvStockItem;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -17,8 +18,21 @@ public class SmartStockService {
         try {
             var items = reportService.readStockReport(reportPath);
 
+            items.forEach(item -> {
+                if (item.getQuantity() < item.getReorderTheshold()) {
+                    var reorderQuantity = calculateReorderQuantity(item);
+
+                }
+            });
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+    private Integer calculateReorderQuantity(CsvStockItem item) {
+        return item.getReorderTheshold() + ((int) Math.ceil(item.getQuantity() * 0.2));
+    }
 }
+
+
